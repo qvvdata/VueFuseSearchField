66650b4930f2b01557a92b6704fef06d334c2c38
fin <fin+github@xbhd.org>
2020-04-30 13:38:41 +0200

    initial commit

---
+++ src/VueFuse.vue
@@ -0,0 +1,234 @@
+<template>
+  <input
+    type="search"
+    ref="search_field"
+    v-model="value"
+    :placeholder="placeholder"
+    :class="{active: is_active, qvvdatasearchfield: true}"
+    @focus="activate"
+    @keydown="forwardArrowkey"
+    @input="value=$event.target.value"
+    @blur="deactivate"
+    :id="input_id?input_id:undefined"
+    >
+</template>
+<script>
+// originally taken from https://github.com/shayneo/vue-fuse/blob/master/src/components/VueFuse.vue at 20a56de
+import Fuse from 'fuse.js';
+
+export default {
+  name: 'VueFuse',
+  data () {
+    return {
+      fuse: null,
+      value: '',
+      result: [],
+      is_active: false,
+    }
+  },
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    search: {
+      type: String,
+      default: ''
+    },
+    eventName: {
+      type: String,
+      default: 'fuseResultsUpdated'
+    },
+    inputChangeEventName: {
+      type: String,
+      default: 'fuseInputChanged'
+    },
+    defaultAll: {
+      type: Boolean,
+      default: true
+    },
+    list: {
+      type: Array
+    },
+    caseSensitive: {
+      type: Boolean,
+      default: false
+    },
+    includeScore: {
+      type: Boolean,
+      default: true
+    },
+    includeMatches: {
+      type: Boolean,
+      default: true
+    },
+    tokenize: {
+      type: Boolean,
+      default: false
+    },
+    matchAllTokens: {
+      type: Boolean,
+      default: false
+    },
+    findAllMatches: {
+      type: Boolean,
+      default: false
+    },
+    id: {
+      type: String,
+      default: ''
+    },
+    shouldSort: {
+      type: Boolean,
+      default: true
+    },
+    threshold: {
+      type: Number,
+      default: 0.6
+    },
+    location: {
+      type: Number,
+      default: 0
+    },
+    distance: {
+      type: Number,
+      default: 100
+    },
+    maxPatternLength: {
+      type: Number,
+      default: 32
+    },
+    minMatchCharLength: {
+      type: Number,
+      default: 1
+    },
+    keys: {
+      type: Array
+    },
+    input_id: {
+      type: String
+    }
+  },
+  computed: {
+    options () {
+      var options = {
+        caseSensitive: this.caseSensitive,
+        includeScore: this.includeScore,
+        includeMatches: this.includeMatches,
+        tokenize: this.tokenize,
+        matchAllTokens: this.matchAllTokens,
+        findAllMatches: this.findAllMatches,
+        shouldSort: this.shouldSort,
+        threshold: this.threshold,
+        location: this.location,
+        distance: this.distance,
+        maxPatternLength: this.maxPatternLength,
+        minMatchCharLength: this.minMatchCharLength,
+        keys: this.keys
+      }
+      if (this.id !== '') {
+        options.id = this.id
+      }
+      return options
+    }
+  },
+  watch: {
+    list () {
+      this.fuse.list = this.list
+      this.fuseSearch()
+    },
+    search () {
+      this.value = this.search;
+    },
+    value () {
+      this.$parent.$emit(this.inputChangeEventName, this.value)
+      this.$emit(this.inputChangeEventName, this.value)
+      this.fuseSearch()
+    },
+    result () {
+      this.$emit(this.eventName, this.result)
+      this.$parent.$emit(this.eventName, this.result)
+    }
+  },
+  methods: {
+    initFuse () {
+      this.fuse = new Fuse(this.list, this.options)
+      if (this.defaultAll) {
+        this.result = this.list
+      }
+      if (this.search) {
+        this.value = this.search
+      }
+    },
+    fuseSearch () {
+      if (this.value.trim() === '') {
+        if (this.defaultAll) {
+          this.result = this.list
+        } else {
+          this.result = []
+        }
+      } else {
+        const r = this.fuse.search(this.value.trim());
+        this.result = r
+          .filter(d => this.value.trim().length>5 || d.score < 0.2)
+          .map(d => d.item);
+      }
+    },
+    activate () {
+      this.is_active = true;
+      this.fuseSearch();
+    },
+    deactivate () {
+      this.is_active = false;
+    },
+    clear () {
+      this.value = "";
+    },
+    forwardArrowkey (event) {
+      if(event.keyCode==40) {
+        this.$emit('arrowkeys', 'down');
+        event.preventDefault()
+        return;
+      }
+
+      if(event.keyCode==38) {
+        this.$emit('arrowkeys', 'up');
+        event.preventDefault()
+        return;
+      }
+
+      if(event.keyCode==13) {
+        this.$emit('arrowkeys', 'enter');
+        event.preventDefault()
+        return;
+      }
+    }
+  },
+  /**
+  * Vue 1.x
+  */
+  ready () {
+    this.initFuse()
+  },
+  /**
+  * Vue 2.x
+  */
+  mounted () {
+    this.initFuse()
+  }
+}
+</script>
+
+<style >
+.qvvdatasearchfield::placeholder {
+  font-weight: normal;
+  color: black;
+  -webkit-text-fill-color: black;
+  font-family: Sailec, sans-serif;
+}
+.qvvdatasearchfield.active::placeholder {
+  font-weight: normal;
+  color: grey;
+  -webkit-text-fill-color: grey;
+}
+</style>
---
+++ src/VueFuseSearchField.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="vuefusesearchfield">
+        <VueFuse
+            input_id="qvv_bezirke_input"
+            :placeholder="placeholder"
+            :list="list"
+            :keys="search_keys"
+            @fuseResultsUpdated="results=$event"
+            :defaultAll="defaultAll"
+            ref="vue_search"
+            @arrowkeys="arrowkeys">
+        </VueFuse>
+        <div class="results" v-if="results.length>0">
+            <div
+                :class="{r:true, highlight: index==highlight_index}"
+                v-for="(r,index) in results.filter((r, index) => index < 6)"
+                @click="select(r)"
+                @mouseover="highlight_index=index"
+                :key="r.gkz">
+                <slot v-bind:result="r">
+                </slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import VueFuse from './VueFuse.vue';
+
+export default {
+    name: 'VueFuseSearchField',
+    components: {
+        VueFuse
+    },
+    props: [
+        'list',
+        'search_keys',
+        'defaultAll',
+        'placeholder',
+    ],
+    data: () => ({
+        results: [],
+        highlight_index: -1,
+        selected_element: null,
+    }),
+    methods: {
+        arrowkeys(key) {
+            if(key=='down') {
+                this.highlight_index+=1;
+            }
+            if(key=='up') {
+                this.highlight_index-=1;
+            }
+            this.highlight_index = Math.max(Math.min(this.highlight_index, this.results.length-1), -1);
+            if(key=='enter') {
+                this.select(this.results[this.highlight_index]);
+                this.$refs.vue_search.$refs.search_field.blur();
+            }
+        },
+        select(r) {
+            this.selected_element=r;
+            this.results=[];
+            this.$refs['vue_search'].clear();
+            this.highlight_index = -1;
+        }
+    },
+    watch: {
+        selected_element() {
+            this.$emit('selected', this.selected_element);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.vuefusesearchfield .results {
+  width: 100%;
+  max-width: 450px;
+  position: absolute;
+  border: 1px solid #333;
+  border-bottom: none;
+  box-sizing: border-box;
+  color: black;
+  z-index: 100;
+}
+
+.vuefusesearchfield .results .r {
+  border-bottom: 1px solid black;
+  padding: 5px 10px;
+  cursor: pointer;
+  background: rgba(255, 255, 255, 0.8)
+}
+
+.vuefusesearchfield .results >>> .r.highlight {
+  background: #A0968B;
+}
+.vuefusesearchfield >>> input {
+  font-family: Sailec, sans-serif;
+  line-height: 19px;
+  margin-top: -1px;
+}
+.vuefusesearchfield >>> input::placeholder {
+  font-family: Sailec, sans-serif;
+}
+</style>
